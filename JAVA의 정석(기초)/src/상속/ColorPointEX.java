f9f81e6cb541f21fc576a35fd052d7d69b77851a
DESKTOP-1L7397I\82105 <w2117@e-mirim.hs.kr>
2022-03-27 15:26:19 +0900

    클래스 변수 및 상속

---
+++ JAVA의 정석(기초)/src/상속/ColorPointEX.java
@@ -0,0 +1,15 @@
+package »ó¼Ó;
+
+public class ColorPointEX {
+	public static void main(String[] args) {
+		Point p = new Point();
+		p.set(1,2);
+		p.showPoint();
+		
+		ColorPoint cp = new ColorPoint();
+		cp.set(3,4);
+		cp.setColor("red");
+		cp.showColorPoint();
+	}
+
+}
---
+++ JAVA의 정석(기초)/src/클래스변수/CurrencyConverter.java
@@ -0,0 +1,30 @@
+package 클래스변수;
+
+import java.util.Scanner;
+
+/*
+ * static변수를 이용하여 달러와 완화를 변환 해주는 환율 계산기를 만들어보자.
+ */
+public class CurrencyConverter {
+	private static double rate; //한국 원화에 대한 환율
+	public static double toDollar(double won) {
+		return won/rate; //한국 원화를 달러로 변환
+	}
+	public static double toKWR(double dollar) {
+		return dollar * rate;  //달러를 한국 원화로 변환
+	}
+	public static void setRate(double r ) {
+		rate = r; //환율설정 . KWR / $1
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("환율(1달러) >> ");
+		double rate = scanner.nextDouble();
+		CurrencyConverter.setRate(rate);
+		System.out.println("백만원은 $ "+CurrencyConverter.toDollar(1000000)+"입니다.");
+		System.out.println("$100는 "+CurrencyConverter.toKWR(100)+"입니다.");
+		scanner.close();
+	}
+
+}
